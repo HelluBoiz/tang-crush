b922d1985574eab1012743b1a125530b7549ae8b
HelluBoiz <86915204+HelluBoiz@users.noreply.github.com>
2021-07-04 20:00:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Nhii ah !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Game khongg :))',
+    desc: 'Game diiiiiii :33 ',
+    btnYes: 'Ok lun nàa ',
+    btnNo: 'Khôngg =)))) :3',
+    question:'Ăn j chx mà Game rùi iii',
+    btnReply: 'Gửi cho Phongzz ',
+    reply: 'Dzoo gameeeee',
+    mess: 'Uh , bít gòi , Dzoo ',
+    messDesc: 'Dzoo h nha :)).',
+    btnAccept: 'Okiiiii lunn :33 ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
