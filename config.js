ec5db7e42f448fc75aa2c65239f689ff66b77f6b
HelluBoiz <86915204+HelluBoiz@users.noreply.github.com>
2021-07-04 19:30:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,14 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Nhiii ah :33',
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    title: 'Game khongg 😙😙😙',
+    desc: 'Game diiiiii :33 ',
+    btnYes: 'Uhm , Chơi lunn :>>>',
+    btnNo: 'Khôngg , No , Đíu chs âu :3 :3'
+    question:'Đang lmj ó mà sai chs game rùi :>>',
+    btnReply: 'Gửi cho Phonggz :33',
+    reply: 'Kệ ik :>  Dzô gameeeeeee :33',
+    mess: 'Mình bt mà :33  ',
+    messDesc: 'Chơi h lun nhaaaaaa <33',
+    btnAccept: 'Okiiiii lun :33',
+    messLink: 'https://www.facebook.com/nhaphong.nguyenduy.3/'
